19e90c0a68f152e3ec3e55bd7868502a97e90b37
SultanFranco98 <s.syidaliev@gmail.com>
2021-01-31 21:24:26 +0600

    first commit, connect to db file

---
+++ db.py
@@ -0,0 +1,16 @@
+from decouple import config
+import psycopg2
+
+DB_NAME = config('DB_NAME')
+DB_HOST = config('DB_HOST')
+DB_PORT = config('DB_PORT')
+DB_USER = config('DB_USER')
+DB_PASSWORD = config('DB_PASSWORD')
+
+try:
+    conn = psycopg2.connect(
+        "dbname={} user={} host={} password={} port={}".format(DB_NAME, DB_USER, DB_HOST, DB_PASSWORD, DB_PORT))
+    print(conn)
+    print("connected to the database")
+except:
+    print("unable to connect to the database")
