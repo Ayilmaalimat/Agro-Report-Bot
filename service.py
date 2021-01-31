14dd8b3a551d2e00559bbebbb0f13dc37a00b621
SultanFranco98 <s.syidaliev@gmail.com>
2021-01-31 21:33:41 +0600

    added late statistics

---
+++ service.py
@@ -0,0 +1,18 @@
+import db
+import datetime
+
+
+def added_late_statistic(result) -> list:
+    """  """
+    time = datetime.time(9, 30)
+    response = []
+    count = 0
+    for row in result:
+        response.append(list(row))
+        response[count].append('Не опоздал')
+        if response[count][3] is not None and response[count][3] > time:
+            response[count][5] = 'Опоздал'
+        if response[count][3] is None:
+            response[count][5] = 'Еще не пришел'
+        count += 1
+    return response
